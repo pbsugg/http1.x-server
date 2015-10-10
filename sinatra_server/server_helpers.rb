87d3d463f7cf0aab895e8434d120e8c8f04fe091
Philip Sugg <pbensugg@gmail.com>
2015-10-10 14:17:38 -0700

    add http request header parsing regex methods

--- sinatra_server/server_helpers.rb
+++ sinatra_server/server_helpers.rb
@@ -5,6 +5,10 @@ def normalize_resource
 
 end
 
-def parse_http_request
-  http_verb
+def get_http_verb(request_header)
+  http_verb = /^[A-Z]*/.match(request_header).to_s
+end
+
+def get_http_resource(request_header)
+  http_resource = /\/[\S]*/.match(request_header)[0]
 end
